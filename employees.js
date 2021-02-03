b061640542cc9cd0018d4abd2130d466571d084f
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-03 00:29:21 -0500

    wrote start script and watch script to package

--- employees.js
+++ employees.js
@@ -19,6 +19,6 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    start();
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
   });
