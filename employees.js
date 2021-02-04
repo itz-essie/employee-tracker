91ebec260dd19d2589e138968a726b778a49cdb8
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-03 20:37:19 -0500

    added connection file with my password and added it to git ignore file

--- employees.js
+++ employees.js
@@ -1,25 +1,10 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const cTable = require('console.table');
-
-// create the connection information for the sql database
-var connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "",
-  database: "employee_trackerDB"
-});
+const connection = require("./connection");
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
