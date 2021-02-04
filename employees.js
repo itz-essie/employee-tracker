da1cc2148dfc9cc1c3d1e6137838e63ab036a677
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-03 22:35:31 -0500

    created functions to view departments, roles, and emplpyess as well as began writing my questions

--- employees.js
+++ employees.js
@@ -8,13 +8,14 @@ const connection = require("./connection");
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  connection.end();
 });
 
 // function to view all departments
 function viewAllDepartments() {
   connection.query("select * from department", function (err, res) {
     if (err) throw err;
-    console.table(res);
+    // console.table(res);
   });
 }
 
@@ -22,17 +23,29 @@ function viewAllDepartments() {
 function viewAllRoles() {
   connection.query("select * from role", function (err, res) {
     if (err) throw err;
-    console.table(res);
+    // console.table(res);
   });
 }
 // function to view all employees
 function viewAllEmployees() {
   connection.query("select * from employee", function (err, res) {
     if (err) throw err;
-    console.table(res);
+    // console.table(res);
   });
 }
 
 viewAllDepartments();
 viewAllRoles();
 viewAllEmployees();
+
+const givenOptions = () => {
+  inquirer
+    .prompt ([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "doSomething",
+      },
+
+    ])
+}
