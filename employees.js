9929971661ff06be0f4b550e69714cf254fd02ce
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-03 23:50:53 -0500

    created functions for users ro do different things, tested it out and the questions appear.

--- employees.js
+++ employees.js
@@ -3,11 +3,21 @@
 var inquirer = require("inquirer");
 const cTable = require("console.table");
 const connection = require("./connection");
+var figlet = require("figlet");
+
+// figlet("Welcome to CMS", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  // console.log("connected as id " + connection.threadId);
   connection.end();
 });
 
@@ -40,12 +50,114 @@ viewAllEmployees();
 
 const givenOptions = () => {
   inquirer
-    .prompt ([
+    .prompt([
       {
         type: "list",
         message: "What would you like to do?",
-        name: "doSomething",
+        name: "userChoice",
+        choices: ["Add", "View", "Update", "Delete"],
       },
-
     ])
-}
+    .then((response) => {
+      switch (response.userChoice) {
+        case "Add":
+          return addWhat();
+        case "View":
+          return viewWhat();
+        case "Update":
+          return updateWhat();
+        case "Delete":
+          return deleteWhat();
+      }
+    });
+};
+
+const addWhat = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please choose what you would like to add.",
+        name: "toAdd",
+        choices: ["Department", "Role", "Employee"],
+      },
+    ])
+    .then((response) => {
+      switch (response.userChoice) {
+        case "Department":
+          return addDepartment();
+        case "Role":
+          return addRole();
+        case "Employee":
+          return addEmployee();
+      }
+    });
+};
+
+const viewWhat = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to view?",
+        name: "toView",
+        choices: ["Department", "Role", "Employee"],
+      },
+    ])
+    .then((response) => {
+      switch (response.userChoice) {
+        case "Department":
+          return viewDepartment();
+        case "Role":
+          return viewRole();
+        case "Employee":
+          return viewEmployee();
+      }
+    });
+};
+
+const updateWhat = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What category would you like to make an update to?",
+        name: "toUpdate",
+        choices: ["Department", "Role", "Employee"],
+      },
+    ])
+    .then((response) => {
+      switch (response.userChoice) {
+        case "Department":
+          return updateDepartment();
+        case "Role":
+          return updateRole();
+        case "Employee":
+          return updateEmployee();
+      }
+    });
+};
+
+const deleteWhat = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What category would you like to delete an item from?",
+        name: "toDelete",
+        choices: ["Department", "Role", "Employee"],
+      },
+    ])
+    .then((response) => {
+      switch (response.userChoice) {
+        case "Department":
+          return deleteDepartment();
+        case "Role":
+          return deleteRole();
+        case "Employee":
+          return deleteEmployee();
+      }
+    });
+};
+
+givenOptions();
