48c776a2ab6a365e18b748dd8b693f47c053b361
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-06 20:22:43 -0500

    added mySql password to connection file using .env

---
+++ connection.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql");
+
+require('dotenv').config();
+
+console.log(process.env.SECRETPASSWORD);
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: process.env.SECRETPASSWORD,
+  database: "employee_trackerDB"
+});
+
+module.exports = connection;
--- employees.js
+++ employees.js
@@ -5,7 +5,6 @@ const cTable = require("console.table");
 const connection = require("./connection");
 var figlet = require("figlet");
 
-
 // figlet("Welcome to CMS", function (err, data) {
 //   if (err) {
 //     console.log("Something went wrong...");
@@ -18,6 +17,7 @@ var figlet = require("figlet");
 // connect to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err;
+  
   // console.log("connected as id " + connection.threadId);
 });
 
@@ -45,11 +45,6 @@ function viewAllEmployees() {
   });
 }
 
-// viewAllDepartments();
-// viewAllRoles();
-// viewAllEmployees();
-
-
 const givenOptions = () => {
   inquirer
     .prompt([
@@ -57,139 +52,166 @@ const givenOptions = () => {
         type: "list",
         message: "What would you like to do?",
         name: "userChoice",
-        choices: ["View All Employees", "Edit Employee Info", "View Roles", "Edit Roles", "View Departments", "Add Departments", "Finished"]
+        choices: [
+          "View All Employees",
+          "Edit Employee Info",
+          "View Roles",
+          "Edit Roles",
+          "View Departments",
+          "Add Departments",
+          "Delete",
+          "Finished",
+        ],
       },
     ])
     .then((response) => {
       switch (response.userChoice) {
         case "View All Employees":
-          return viewAllEmployees();
+          viewAllEmployees();
+          break;
         case "Edit Employee Info":
-          return editEmployeeInfo();
+          editEmployeeInfo();
+          break;
         case "View Roles":
-          return viewAllRoles();
+          viewAllRoles();
+          break;
         case "Edit Roles":
-          return editRoles();
+          editRoles();
+          break;
         case "View Departments":
-          return viewAllDepartments();
+          viewAllDepartments();
+          break;
         case "Add Departments":
-          return addDepartments();
+          addDepartments();
+          break;
+        case "Delete":
+          Delete();
         case "Finished":
-          return finished();
+          finished();
       }
     });
 };
 const whatNow = () => {
-  inquirer.prompt([
-    {
-      name: "whatNow",
-      type: "confirm",
-      message: "Anything else?",
-    }
-  ]).then((response) => {
-    if (response.whatNow === "Yes"){
-      return givenOptions();
-    } else {
-        return finished()
-  }
-  })
-}
+  inquirer
+    .prompt([
+      {
+        name: "whatNow",
+        type: "confirm",
+        message: "Anything else?",
+      },
+    ])
+    .then((response) => {
+      if (response.whatNow === "Yes") {
+        return givenOptions();
+      } else {
+        return finished();
+      }
+    });
+};
 
 givenOptions();
 
 function confirmString(string) {
-  if ((string.trim() != "") && (string.trim().length <= 30)) {
-      return true;
+  if (string.trim() != "" && string.trim().length <= 30) {
+    return true;
   }
-  return "Invalid input. Please limit your input to 30 characters or less."
-};
-
+  return "Please limit your input to 30 characters or less.";
+}
 
 function editEmployeeInfo() {
   connection.query("SELECT * FROM employee", function (err, res) {
     if (err) throw err;
-    return res
-    })
-  inquirer.prompt({
+    return res;
+  });
+  inquirer
+    .prompt({
       name: "editOption",
       type: "list",
       message: "What would you like to update?",
       choices: [
-          "Change Employee Salary",
-          "Change Employee Role",
-          "Change Employee Manager",
-          "Remove Employee",
-          "Return to Menu"
-      ]
-  }).then(response => {
+        "Change Employee Salary",
+        "Change Employee Role",
+        "Change Employee Manager",
+        "Remove Employee",
+        "Return to Menu",
+      ],
+    })
+    .then((response) => {
       switch (response.editOption) {
-          case "Change Employee Salary":
-              changeSalary();
-              break;
-          case "Change Employee Role":
-              updateEmployeeRole();
-              break;
-          case "Change Employee Manager":
-              updateManager();
-              break;
-          case "Remove Employee":
-              removeEmployee();
-              break;
-          case "Return to Menu":
-              givenOptions();
-              break;
+        case "Change Employee Salary":
+          changeSalary();
+          break;
+        case "Change Employee Role":
+          updateEmployeeRole();
+          break;
+        case "Change Employee Manager":
+          updateManager();
+          break;
+        case "Remove Employee":
+          removeEmployee();
+          break;
+        case "Return to Menu":
+          givenOptions();
+          break;
       }
-  })
-};
+    });
+}
 
-function editRoles(){
+function editRoles() {
   connection.query("SELECT * FROM role", function (err, res) {
     if (err) throw err;
     const roleInfo = res;
     const roleNames = roleInfo.map((roleItem) => {
       return roleItem.title;
-    })
-    inquirer.prompt([
-      {
-        type:"list",
-        message: "Which role would you like to edit?",
-        name: "newRole",
-        choices: roleNames
-      }, 
-
-    ]).then((answers) => {
-      console.log(answers);
-    })
+    });
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Which role would you like to edit?",
+          name: "newRole",
+          choices: roleNames,
+        },
+      ])
+      .then((answers) => {
+        console.log(answers);
+      });
     // either edit entire entry (title, salary) pull departments, and list those ids
   });
 }
 
-function addDepartments(){
-  inquirer.prompt([
-    {
+function addDepartments() {
+  inquirer
+    .prompt([
+      {
         name: "departName",
         type: "input",
         message: "Enter new department:",
-        validate: confirmString
-    }
-]).then(answers => {
-    connection.query("INSERT INTO department (name) VALUES (?)", [answers.departName]);
-    console.log(`${answers.departName} was added to departments.`);
-    givenOptions();
-})
-};
-// /Build a command-line application that at a minimum allows the user to:
+        validate: confirmString,
+      },
+    ])
+    .then((answers) => {
+      connection.query("INSERT INTO department (name) VALUES (?)", [
+        answers.departName,
+      ]);
+      console.log(`${answers.departName} was added to departments.`);
+      givenOptions();
+    });
+}
 
+
+
+
+function deleteCatgeory(){
+
+}
+// /Build a command-line application that at a minimum allows the user to:
 
 // * Add departments, roles, employees
 
-
 // * View departments, roles, employees
 
-                                               
-
 // * Update employee roles
-
 
 // Bonus points if you're able to:
 
@@ -197,18 +219,22 @@ function addDepartments(){
 
 // * View employees by manager
 
-// * Delete departments, roles, and employees   
+// * Delete departments, roles, and employees
 
 // * View the total utilized budget of a department -- ie the combined salaries of all employees in that department
 
 finished = () => {
-  figlet('Goodbye!', function(err, data) {
-  if (err) {
-      console.log('Something went wrong...');
+  figlet("Goodbye!", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
       console.dir(err);
       return;
-  }
-  console.log(data)
-  connection.end(); //close the connection
-});
-}
+    }
+    console.log(data);
+    connection.end(); //close the connection
+  });
+};
+
+// viewAllDepartments();
+// viewAllRoles();
+// viewAllEmployees();
